9976b87eb64e1e4a74e9907d4507c016c17f6219
Denis Gontcharov <49554133+gontcharovd@users.noreply.github.com>
2024-05-03 13:26:58 +0200

    Update configure-vm.ps1

--- src/configure-vm.ps1
+++ src/configure-vm.ps1
@@ -5,8 +5,14 @@ Install-WindowsFeature -Name "RSAT-RemoteAccess-Powershell"
 Install-RemoteAccess -VpnType RoutingOnly
 Get-NetAdapter | Set-NetIPInterface -Forwarding Enabled
 
+
+Set-Content -Path "$HOME\Desktop\hello1.txt" -Value "part one"
+
 # Install "Hello World" webserver
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
-remove-item \'C:\\inetpub\\wwwroot\\iisstart.htm\'
-Add-Content -Path \'C:\\inetpub\\wwwroot\\iisstart.htm\' `
-	-Value $(\'Hello World from \' + $env:computername)
+remove-item 'C:\inetpub\wwwroot\iisstart.htm'
+Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' `
+	-Value $('Hello World from ' + $env:computername)
+
+
+Set-Content -Path "$HOME\Desktop\hello2.txt" -Value "part two"
